6ad637646a5a444dd6099d76ac02d50c80ae606d
bamsmart <bamsmart@github.com>
2019-03-18 17:46:39 +0700

    # WARNING: head commit changed in the meantime
    ok

--- application/views/pages/forms/dimensi_follow_up.php
+++ application/views/pages/forms/dimensi_follow_up.php
@@ -1,20 +1,22 @@
-	<!-- Content Header (Page header) -->
-	<section class="content-header">
-		<h1>
-			Pengujian Pemeriksaaan dan Dimensi Sudut Kusut Kain  <!-- <small>Preview</small> -->
-		</h1>
-		<ol class="breadcrumb">
-			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-			<li><a href="#">Forms</a></li>
-			<li class="active">Advanced Elements</li>
-		</ol>
-	</section>
 
-	<!-- Main content -->
-	<section class="content">
+<!-- Content Header (Page header) -->
+<section class="content-header">
+	<h1>
+		Pengujian Pemeriksaaan dan Dimensi Sudut Kusut Kain
+		<!-- <small>Preview</small> -->
+	</h1>
+	<ol class="breadcrumb">
+		<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		<li><a href="#">Forms</a></li>
+		<li class="active">Advanced Elements</li>
+	</ol>
+</section>
 
-		<div class="box box-default">
-			<!-- <div class="box-header with-border">
+<!-- Main content -->
+<section class="content">
+
+	<div class="box box-default">
+		<!-- <div class="box-header with-border">
 				<h3 class="box-title">Form Pengujian Fisika</h3>
 
 				<div class="box-tools pull-right">
@@ -27,144 +29,184 @@
 					</button>
 				</div>
 			</div> -->
-			<!-- /.box-header -->
-			  <form >
+		<!-- /.box-header -->
+		<form>
 			<div class="box-body">
-			
-			
-			<!-- title row -->
-      <div class="row">
-        <div class="col-xs-12">
-          <h2 class="page-header">
-            <i class="fa fa-globe"></i> PT ShinesDev Indonesia
-            <small class="pull-right">Date: 10/03/2019</small>
-          </h2>
-        </div>
-        <!-- /.col -->
-      </div>
-      
-      <!-- info row -->
-      <div class="row invoice-info">
-        <div class="col-sm-4 invoice-col">
-          No. Work Order
-          <address>
-            <strong>356/04/III/19</strong><br>
-            795 Folsom Ave, Suite 600<br>
-            San Francisco, CA 94107<br>
-            Phone: (804) 123-5432<br>
-            Email: info@almasaeedstudio.com
-          </address>
-        </div>
-        <!-- /.col -->
-        <div class="col-sm-4 invoice-col">
-          To
-          <address>
-            <strong>John Doe</strong><br>
-            795 Folsom Ave, Suite 600<br>
-            San Francisco, CA 94107<br>
-            Phone: (555) 539-1037<br>
-            Email: john.doe@example.com
-          </address>
-        </div>
-        <!-- /.col -->
-        <div class="col-sm-4 invoice-col">
-          <b>Invoice #007612</b><br>
-          <br>
-          <b>Order ID:</b> 4F3S8J<br>
-          <b>Payment Due:</b> 2/22/2014<br>
-          <b>Account:</b> 968-34567
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
+				<!-- title row -->
 				<div class="row">
-					<div class="col-md-6">
-
-						<!-- Date -->
-						<div class="form-group">
-						
-						<div class="row">
-						  <div class="col-xs-4">
-							<label>Tanggal:</label>
-							<div class="input-group date">
-								<div class="input-group-addon">
-									<i class="fa fa-calendar"></i>
-								</div>
-								<input type="text" class="form-control pull-right"
-									id="datepicker">
-							</div>
-						  </div>
-						  
-						   <div class="col-xs-4">
-						   <label>Jenis: </label>
-						       <select
-								class="form-control select2" style="width: 100%;">
-								<option selected="selected">Kain</option>
-								<option>Benang</option>
-							</select>
-						   </div>
-						   
-						   <div class="col-xs-4">
-						   <label>Jumlah: </label>
-						       <input type="text" class="form-control" placeholder="Enter ...">
-						   </div>
-						</div>
-						</div>
-
-						<div class="form-group">
-							<label>Nama Perusahaan: </label> <select
-								class="form-control select2" style="width: 100%;">
-								<option selected="selected">PT ShinesDev Indonesia</option>
-								<option>PT Sejahtera Mandiri</option>
-								<option>PT Rumingkang Indonesia</option>
-								<option>PT Harapan Jaya Sakti</option>
-								<option>PT Urban Texstil Jaya</option>
-								<option>PT Kembang Wangi Jaya</option>
-								<option>PT Rembaka Indonesia</option>
-							</select>
-						</div>
-					</div>
-					<!-- /.col -->
-
-					<div class="col-md-6">
-						<div class="form-group">
-							<label>Jenis Pengujian: </label> <select class="form-control select2"
-								multiple="multiple" data-placeholder="Select a State"
-								style="width: 100%;">
-								<option>Pengujian Pemeriksaaan dan Dimensi Sudut Kusut Kain</option>
-								<option>Pengujian Tetal Benang dan Anyaman Kain</option>
-								<option>Pengujian Nomor Benang</option>
-								<option>Pengujian Kekuatan Tarik</option>
-								<option>Pengujian Kekuatan Sobek (Elmendorf)</option>
-								<option>Pengujian Daya Tembus Udara</option>
-								<option>Pengujian Kain Twist</option>
-							</select>
-						</div>
+					<div class="col-xs-12">
+						<h2 class="page-header">
+							<i class="fa fa-globe"></i> PT ShinesDev Indonesia <small
+								class="pull-right">Date: 10/03/2019</small>
+						</h2>
 					</div>
 					<!-- /.col -->
 				</div>
-				<!-- /.row -->
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="box-header">
+							<h3 class="box-title">Dimensi Kain</h3>
+						</div>
+
+						<table class="table table-bordered">
+							<tr>
+								<th width="150px"></th>
+								<th>(1)</th>
+								<th>(2)</th>
+								<th>(3)</th>
+								<th>(4)</th>
+								<th>(5)</th>
+								<th>&Sigma;</th>
+							</tr>
+							<tr>
+								<th width="150px">Lebar (m)</th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control" placeholder="Sum"
+									disabled></th>
+							</tr>
+							<tr>
+								<th width="150px">Panjang (m)</th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control" placeholder="Sum"
+									disabled></th>
+							</tr>
+							<tr>
+								<th width="150px">Berat 100 cm<sup>2</sup>(g)
+								</th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control" placeholder="Sum"
+									disabled></th>
+							</tr>
+							<th colspan="7">Berat Per m<sup>2</sup> = 99.76 (fk) x
+							</th>
+							</tr>
+						</table>
+					</div>
+					
+					
+					<div class="col-xs-12">
+						<div class="box-header">
+							<h3 class="box-title">Sudut Kusut</h3>
+						</div>
+
+						<table class="table table-bordered">
+							<tr>
+								<th width="80px" colspan="2"></th>
+								<th>(1)</th>
+								<th>(2)</th>
+								<th>(3)</th>
+								<th>(4)</th>
+								<th>(5)</th>
+								<th>&Sigma;</th>
+							</tr>
+							<tr>
+								<th width="80px" rowspan="2">Lusi</th>
+								<th width="120px">Antar Muka</th>
+								<th><input type="text" class="form-control"
+									placeholder=""></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control" placeholder="Sum"
+									disabled></th>
+							</tr>
+							<tr>
+								<th width="120px">Antar Belakang</th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+									<th><input type="text" class="form-control"
+									placeholder=""></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control" placeholder="Sum"
+									disabled></th>
+							</tr>
+							
+							<tr>
+								<th width="80px" rowspan="2">Pakan
+								</th>
+								<th width="120px">Antar Muka</th>
+								<th><input type="text" class="form-control"
+									placeholder=""></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control" placeholder="Sum"
+									disabled></th>
+							</tr>
+							<tr><th width="120px">Antar Belakang</th>
+							<th><input type="text" class="form-control"
+									placeholder=""></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control"
+									placeholder=".col-xs-3"></th>
+								<th><input type="text" class="form-control" placeholder="Sum"
+									disabled></th>
+							</tr>
+						</table>
+					</div>
+					
+					
+				</div>
 			</div>
 			<!-- /.box-body -->
 
 			<div class="box-footer">
 				<button type="submit" class="btn btn-primary pull-right">Submit</button>
 			</div>
-</form>
-			<!-- <div class="box-footer">
+		</form>
+		<!-- <div class="box-footer">
 				Visit <a href="https://select2.github.io/">Select2 documentation</a>
 				for more examples and information about the plugin.
 			</div> -->
-		</div>
-		<!-- /.box -->
-	</section>
-	<!-- /.content -->
+	</div>
+	<!-- /.box -->
+</section>
+<!-- /.content -->
